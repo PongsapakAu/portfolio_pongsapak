273739289f75334f2ae81a09dfd783746b1103f4
PongsapakAu <pongsapak.0895@gmail.com>
2025-05-08 11:22:44 +0700

    test

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,6 +3,7 @@ export default function About() {
     <div className="bg-white w-full h-screen flex ">
       <div className="w-full h-auto ">text</div>
       <div>photo</div>
+      <div>test</div>
     </div>
   )
 }
